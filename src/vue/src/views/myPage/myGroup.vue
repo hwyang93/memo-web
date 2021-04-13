1eea7f8da5f2970a0a1fc104a22906d6acf25655
songb <songb@esumtech.com>
2021-04-13 16:19:21 +0900

    템플릿 && 디자인 수정중

--- src/vue/src/views/myPage/myGroup.vue
+++ src/vue/src/views/myPage/myGroup.vue
@@ -1,15 +1,17 @@
 <template>
-  <section class=" container space blog_page">
+
+  <section class="container space-type-1 blog_page">
+    <h1 class="title-type-1">My Group</h1>
     <div class="row">
       <!--blog-sidbar-->
       <div class="col-lg-4 col-md-12 ">
-        <div class="sidebar sidbar_left bg-gray">
+        <div class="sidebar sidbar_left shadow-wrap-1">
           <!--search-->
           <div class="blog-sidebar-widgets">
             <h3 class="widgets-title">  조회 </h3>
             <div class="blog-sidebar-widgets-inner">
               <form class="searchform">
-                <div class="form-group search_2">
+                <div class="form-group search_2 p-0">
                   <input type="text" class="form-control" placeholder="키워드 검색" id="s">
                   <button type="button" class="btn"><i class="fas fa-search"></i></button>
                 </div>
@@ -41,8 +43,8 @@
         </div>
       </div>
       <!--/sidebar-close-->
-      <div class="col-lg-8 col-md-12 blog_page">
-        <button type="button" class="btn"><i class="fas fa-plus-square"></i></button>
+      <div class="col-lg-8 col-md-12 blog_page shadow-wrap-1">
+        <!-- <button type="button" class="btn"><i class="fas fa-plus-square"></i></button>
         <div class="row">
           <div class="blog_post col-lg-6 col-md-6" v-for="item in userScheduleList" :key="item.idx">
             <figure>
@@ -54,7 +56,48 @@
               </figcaption>
             </figure>
           </div>
+        </div> -->
+
+        <!-- <div class="box-wrap"> -->
+
+        <h2></h2>
+        <div class="tab-area-gf">
+          <b-tabs content-class="mt-3" fill>
+            <b-tab title="Group" active>
+              <h3 class="">그룹원 추가</h3>
+
+              <div>
+                <b-button v-b-modal.modal-center>그룹 추가 하기</b-button>
+
+                <b-modal id="modal-center" centered title="그룹에 친구 추가">
+                  <ul class="user-list-area">
+                    <li class="user-list">
+                      <a href="#" class="user-pic">
+                        <img src="../../images/friends/user-sample.jpg" alt="">
+                      </a>
+                      <a href="#" class="user-name">charm_bbong</a>
+                      <button class="btn btn-info" id="btn-add-group">Add</button>
+                    </li>
+                    <li class="user-list">
+                      <a href="#" class="user-pic">
+                        <img src="../../images/friends/user-sample.jpg" alt="">
+                      </a>
+                      <a href="#" class="user-name">charm_bbong</a>
+                      <button class="btn btn-info" id="btn-add-group">Add</button>
+                    </li>
+                  </ul>
+                  <!-- <p class="my-4">Vertically centered modal!</p> -->
+                </b-modal>
+              </div>
+            </b-tab>
+            <b-tab title="Friends">
+              <h3></h3>
+              <p>I'm the second tab</p>
+            </b-tab>
+          </b-tabs>
         </div>
+        <!-- </div> -->
+
       </div>
     </div>
   </section>
@@ -86,3 +129,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  .modal-backdrop {
+    background-color: rgba(0,0,0,.3);
+  }
+</style>
