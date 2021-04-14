ce4eea3a8ea94376cd1e6976e54946d8695a2181
hyeonseo-noh <hseo0112@naver.com>
2021-04-14 16:21:13 +0900

    mySchedule 수정 삭제 modal 작업 중

--- src/main/java/memoWeb/web/mySchedule/repository/MyScheduleRepository.java
+++ src/main/java/memoWeb/web/mySchedule/repository/MyScheduleRepository.java
@@ -1,10 +1,13 @@
 package memoWeb.web.mySchedule.repository;
 
+import java.util.List;
+
 import memoWeb.web.main.domain.UserScheduleVO;
 import memoWeb.web.main.domain.UserVO;
 
-import java.util.List;
-
 public interface MyScheduleRepository {
     List<UserScheduleVO> getUserScheduleListAll (UserVO member);
+	UserScheduleVO getScheduleDetail(int idx);
+	long updateSchedule(UserScheduleVO userScheduleVO);
+	long deleteSchedule(int idx);
 }
--- src/vue/src/views/myPage/mySchedule.vue
+++ src/vue/src/views/myPage/mySchedule.vue
@@ -43,14 +43,14 @@
       <!--/sidebar-close-->
       <div class="col-lg-8 col-md-12 blog_page">
         <div class="row">
-          <div class="blog_post col-lg-6 col-md-6" v-for="item in userScheduleList" :key="item.idx" @click="showModal">
-            <div class="card mb-3" style="height: 350px">
+          <div class="blog_post col-lg-6 col-md-6" v-for="item in userScheduleList" :key="item.idx">
+            <div class="card mb-3" style="height: 350px" @click="showModal(item.idx)">
               <div class="card-header">{{ item.title }}</div>
               <div class="card-body">
                 <h5 class="card-title">주소</h5>
                 <p class="card-text">{{ item.promisePlace }}</p>
                 <h5 class="card-title">일정</h5>
-                <p class="card-text">{{ item.startDate + ' ~ ' + item.endDate }}</p>
+                <p class="card-text">{{ $moment(item.startDate).format('YYYY-MM-DD') + ' ~ ' + $moment(item.endDate).format('YYYY-MM-DD') }}</p>
                 <h5 class="card-title">메모</h5>
                 <p class="card-text">{{ item.memo }}</p>
               </div>
@@ -59,36 +59,142 @@
         </div>
       </div>
 
-      <div class="md-containar" id="modal" v-show="is_show">
-        <div class="scheduleDetail">
-          <div class="md-schedule">
-            <div class="md-b">
-              <div class="md-header"></div>
+      <transition name="modal" v-if="isStatusOn">
+        <div class="modal modal-overlay">
+          <div class="modal-window">
+            <div class="md-map">
+              <div id="map" class="map"></div>
+            </div>
+            <div class="md-content md-default" v-if="isdefault">
+              <div class="md-header">{{ scheduleDetail.title }}</div>
               <div class="md-body">
                 <h5 class="md-title">주소</h5>
-                <p class="md-text"></p>
+                <p class="md-text">{{ scheduleDetail.promisePlace }}</p>
                 <h5 class="md-title">일정</h5>
-                <p class="md-text"></p>
+                <p class="md-text">{{ $moment(scheduleDetail.startDate).format('YYYY-MM-DD') + ' ~ ' + $moment(scheduleDetail.endDate).format('YYYY-MM-DD') }}</p>
                 <h5 class="md-title">메모</h5>
-                <p class="md-text"></p>
+                <p class="md-text">{{ scheduleDetail.memo }}</p>
               </div>
             </div>
-            <div class="md-footer">
-              <b-button>수정</b-button>
-              <b-button>삭제</b-button>
+            <div class="md-content md-modify" v-else-if="isModify">
+              <input type="hidden" v-model="scheduleDetail.idx" />
+              <div class="md-header"><input type="text" class="md-header" v-model="scheduleDetail.title" /></div>
+              <div class="md-body">
+                <h5 class="md-title">주소</h5>
+                <input type="text" class="md-text" v-model="scheduleDetail.promisePlace" />
+                <h5 class="md-title">일정</h5>
+                <input type="text" class="md-text" v-model="scheduleDetail.startDate" /><br />
+                <input type="text" class="md-text" v-model="scheduleDetail.endDate" />
+                <h5 class="md-title">메모</h5>
+                <textarea class="md-text" v-model="scheduleDetail.memo"></textarea>
+              </div>
             </div>
+            <footer class="modal-footer foo-default" v-if="isdefault">
+              <slot name="footer">
+                <button class="ModifyBtn md-btn" @click="modify">Modify</button>
+                <button class="closeBtn md-btn" @click="close">Close</button>
+              </slot>
+            </footer>
+            <footer class="modal-footer foo-modify" v-else-if="isModify">
+              <button class="saveBtn md-btn" @click="save">Save</button>
+              <button class="deleteBtn md-btn" @click="deleteBtn(scheduleDetail.idx)">Delete</button>
+              <button class="cancelBtn md-btn" @click="cancel">Cancel</button>
+            </footer>
           </div>
         </div>
-      </div>
+      </transition>
     </div>
   </section>
 </template>
 <style>
-.md-containar {
-  background-color: rgba(0, 0, 0, 0.3);
+.modal-overlay {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
 }
-.scheduleDetail {
-  width: 40%;
+.modal-window {
+  background: #fff;
+  overflow: hidden;
+  width: 60%;
+  transition: opacity 0.4s, transform 0.4ms;
+}
+.modal-footer {
+  padding: 0.7rem;
+  margin-bottom: 0;
+  background-color: rgba(0, 0, 0, 0.03);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.4s;
+}
+.modal-leave-active {
+  transition: opacity 0.4s ease 0.2s;
+}
+.md-map {
+  float: left;
+  width: 50%;
+  height: 500px;
+}
+.md-content {
+  width: 50%;
+  float: left;
+  height: 500px;
+}
+.md-header {
+  padding: 1rem 2rem;
+  margin-bottom: 0;
+  background-color: rgba(0, 0, 0, 0.03);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+.md-body {
+  flex: 1 1 auto;
+  padding: 1.5rem 2rem;
+}
+.md-title {
+  margin-bottom: 0.75rem;
+}
+.md-text:last-child {
+  margin-bottom: 0;
+}
+.md-btn {
+  border: 0;
+  border-radius: 0.75rem;
+  padding: 0.5rem 1rem 0.5rem 1rem;
+}
+.ModifyBtn {
+  background: #00adb5;
+}
+.saveBtn {
+  background: lightcoral;
+}
+.deleteBtn {
+  background: darkgray;
+}
+.md-modify .md-header {
+  padding: 0.73rem;
+}
+.md-modify .md-header input {
+  padding: 0.2rem;
+  background: #fff;
+  width: 80%;
+  border: 1px solid #ccc;
+  margin-left: 1rem;
+}
+.md-modify .md-text {
+  padding: 0.2rem;
+  margin-bottom: 0.75rem;
+  width: 80%;
+}
+.md-modify textarea {
+  height: 120px;
+  resize: none;
 }
 </style>
 
@@ -96,6 +202,7 @@
 <script>
 import axiosUtil from '@/utils/axios-util';
 import leftMenu from '@/views/myPage/leftMenu';
+
 export default {
   name: 'schedule',
   components: {
@@ -103,14 +210,178 @@ export default {
   },
   data() {
     return {
-      userScheduleList: []
+      localLat: 0,
+      localLng: 0,
+      map: null,
+      clusterer: null,
+      infowindow: null,
+      isStatusOn: false,
+      isdefault: true,
+      isModify: false,
+      userScheduleList: [],
+      scheduleDetail: {}
     };
+  },
+  watch: {
+    localLat: function () {
+      window.kakao && window.kakao.maps ? this.initMap() : this.addScript();
+    }
   },
   methods: {
     getSchedule() {
       axiosUtil.get('/api/mySchedule/getUserScheduleListAll.do', {}, result => {
         this.userScheduleList = result.data.userScheduleList;
-        console.log(this.userScheduleList);
+      });
+    },
+    showModal(idx) {
+      axiosUtil.get('/api/mySchedule/getScheduleDetail.do', { params: { idx: idx } }, result => {
+        this.scheduleDetail = result.data.scheduleDetail;
+      });
+
+      this.isStatusOn = true;
+    },
+    initMap() {
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(this.localLat, this.localLng),
+        level: 3
+      };
+
+      this.map = new kakao.maps.Map(container, options);
+      // 마커가 표시될 위치입니다
+      const markerPosition = new kakao.maps.LatLng(this.localLat, this.localLng);
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        position: markerPosition
+      });
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(this.map);
+
+      this.clusterer = new kakao.maps.MarkerClusterer({
+        map: this.map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel: 7 // 클러스터 할 최소 지도 레벨
+      });
+
+      // 주소-좌표 변환 객체를 생성합니다
+      const geocoder = new kakao.maps.services.Geocoder();
+      this.infowindow = new kakao.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+      // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+      kakao.maps.event.addListener(this.map, 'click', mouseEvent => {
+        this.scheduleDetail.latLng = mouseEvent.latLng;
+        geocoder.coord2Address(mouseEvent.latLng.La, mouseEvent.latLng.Ma, (result, status) => {
+          if (status === kakao.maps.services.Status.OK) {
+            var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+            detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+
+            var content = '<div class="bAddr">' + '<span class="title">법정동 주소정보</span>' + detailAddr + '</div>';
+            this.scheduleDetail.promisePlace = !!result[0].road_address ? result[0].road_address.address_name : result[0].address.address_name;
+            this.scheduleDetail.lon = mouseEvent.latLng.La;
+            this.scheduleDetail.lat = mouseEvent.latLng.Ma;
+            // 마커를 클릭한 위치에 표시합니다
+            marker.setPosition(mouseEvent.latLng);
+            marker.setMap(this.map);
+
+            // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+            this.infowindow.setContent(content);
+            this.infowindow.open(this.map, marker);
+          }
+        });
+      });
+      if (this.scheduleDetail.length > 0) {
+        this.setMarkers();
+      }
+    },
+    addScript() {
+      let script = document.createElement('script');
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=233fb509b41527eb6530cb8ca5b635c5&libraries=services,clusterer,drawing';
+      document.head.appendChild(script);
+    },
+    showPosition(position) {
+      this.localLat = position.coords.latitude;
+      this.localLng = position.coords.longitude;
+    },
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      }
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition(
+      //     function (position) {
+      //       this.latitude = position.coords.latitude;
+      //       this.longitude = position.coords.longitude;
+      //     },
+      //     function (error) {
+      //       console.error(error);
+      //     },
+      // {
+      //   enableHighAccuracy: false,
+      //   maximumAge: 0,
+      //   timeout: Infinity,
+      // }
+      //   );
+      //   console.log(this);
+      // } else {
+      //   alert("GPS를 지원하지 않습니다");
+      // }
+    },
+    modify: function () {
+      this.isdefault = false;
+      this.isModify = true;
+    },
+    save: function () {
+      console.log(this.scheduleDetail);
+      axiosUtil.post('/api/mySchedule/updateSchedule.do', this.scheduleDetail, result => {
+        alert('수정되었습니다.');
+        this.isStatusOn = false;
+        this.isdefault = true;
+        this.isModify = false;
+        this.getSchedule();
+      });
+    },
+    deleteBtn(idx) {
+      axiosUtil.get('/api/mySchedule/deleteSchedule.do', { params: { idx: idx } }, result => {
+        alert('삭제되었습니다.');
+        this.isStatusOn = false;
+        this.isdefault = true;
+        this.isModify = false;
+        this.getSchedule();
+      });
+    },
+    cancel: function () {
+      this.isdefault = true;
+      this.isModify = false;
+    },
+    close: function () {
+      this.isStatusOn = false;
+    },
+    setMarkers() {
+      let markers = this.userScheduleList.map(item => {
+        return new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(item.lat, item.lon)
+        });
+      });
+      this.clusterer.addMarkers(markers);
+
+      markers.map((marker, index) => {
+        let content = '<div>장소 : ' + this.scheduleDetail.promisePlace + '</div>';
+        content += '<div>일정 : ' + this.scheduleDetail.startDate + ' ~ ' + this.scheduleDetail.endDate + '</div>';
+        content += '<div>일정 : ' + this.scheduleDetail.memo + '</div>';
+        // let infowindow = new kakao.maps.InfoWindow({
+        //   content : content
+        // });
+
+        // 마커에 마우스오버 이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'mouseover', () => {
+          this.infowindow.setContent(content);
+          this.infowindow.open(this.map, marker);
+        });
+
+        // 마커에 마우스오버 이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'mouseout', () => {
+          this.infowindow.close();
+        });
       });
     }
   },
