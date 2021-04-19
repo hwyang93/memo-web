5e97849013285054d1ce27dfaf585b5d5c7f81ab
hwyang93 <yanghw0911@gmail.com>
2021-04-19 18:05:54 +0900

    - 메인 화면 지도 검색기능 개발중 - DTO클래스 적용 및 projections.fields 적용

---
+++ src/main/java/memoWeb/web/main/domain/UserDTO.java
@@ -0,0 +1,24 @@
+package memoWeb.web.main.domain;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String userId;
+    private String userPassword;
+    private String userEmail;
+    private String userName;
+    private String regDate;
+    private String openFlag;
+
+    @QueryProjection
+    public UserDTO(String userId, String userPassword, String userEmail, String userName, String regDate, String openFlag) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.regDate = regDate;
+        this.openFlag = openFlag;
+    }
+}
