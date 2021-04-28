1aab2fbdf12f03364ceaff532c6a40c32f0642ad
hwyang93 <yanghw0911@gmail.com>
2021-04-28 09:00:05 +0900

    userVO -> userDTO로 변경

--- src/main/java/memoWeb/web/mySchedule/controller/MyScheduleContoller.java
+++ src/main/java/memoWeb/web/mySchedule/controller/MyScheduleContoller.java
@@ -34,8 +34,8 @@ public class MyScheduleContoller {
 	@GetMapping("getUserScheduleListAll.do")
 	public Map<String, Object> getUserScheduleListAll(Model model, UserScheduleVO userScheduleVo, HttpSession session) {
 		Map<String, Object> map = new HashMap<>();
-		UserVO member = (UserVO) session.getAttribute(CommonConstants.SESSION);
-		List<UserScheduleVO> userScheduleList = myScheduleService.getUserScheduleListAll(member);
+		UserDTO user = (UserDTO) session.getAttribute(CommonConstants.SESSION);
+		List<UserScheduleVO> userScheduleList = myScheduleService.getUserScheduleListAll(user);
 		map.put("userScheduleList", userScheduleList);
 		return map;
 	}
