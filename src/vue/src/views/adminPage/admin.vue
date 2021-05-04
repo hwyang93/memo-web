2f8a7f62bd47f86d8ba98a54edc173c8e16eee3a
hyeonseo-noh <hseo0112@naver.com>
2021-05-04 17:46:44 +0900

    추가수정

--- src/vue/src/views/adminPage/admin.vue
+++ src/vue/src/views/adminPage/admin.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container home-banner-container space-type-1">
     <div class="row home-banner-row shadow-wrap-1">
-      <div class="title-back">
-        <p class="title">회원관리</p>
-      </div>
+      <h3 class="title-back">회원관리</h3>
       <page-link :listArray="userList" style="width: 90%; margin: auto" />
     </div>
   </div>
@@ -32,16 +30,28 @@ export default {
 
 <style scoped>
 .title-back {
-  width: 90%;
-  border-radius: 22px;
-  margin: auto;
+  padding: 8px 30px;
+  color: #fff;
+  overflow: hidden;
+  margin-bottom: 30px;
+  width: 30%;
+  text-transform: capitalize;
+  position: relative;
+  background: linear-gradient(90deg, #00adb5 61%, #00adb5 2%, #00adb5 14%, #9a1f1f00 -8%);
 }
-
-.title {
-  font-size: 22px;
-  padding: 15px 0 0 20px;
+.title-back:after {
+  content: '';
+  background: linear-gradient(90deg, #00adb5 57%, #393d46 13%, #9a1f1f00 37%);
+  position: absolute;
+  width: 68px;
+  top: 0px;
+  height: 100%;
+  transform: rotate(180deg);
+  bottom: 0;
+  right: 28%;
+  border-radius: 50% 0% 0 50%;
+  height: 50px;
 }
-
 .container {
   margin-bottom: 40px;
 }
