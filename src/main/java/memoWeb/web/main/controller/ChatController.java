81cc292ae44b0f57667843b8abf49c5240996084
hwyang93 <yanghw0911@gmail.com>
2021-05-04 14:06:45 +0900

    - 테스트 코드 주석

--- src/main/java/memoWeb/web/main/controller/ChatController.java
+++ src/main/java/memoWeb/web/main/controller/ChatController.java
@@ -1,12 +1,14 @@
 package memoWeb.web.main.controller;
 
 import memoWeb.common.constant.CommonConstants;
-import memoWeb.web.main.domain.*;
+import memoWeb.web.main.domain.ChatRoomDTO;
+import memoWeb.web.main.domain.ChatRoomUserDTO;
+import memoWeb.web.main.domain.ChatVO;
+import memoWeb.web.main.domain.UserDTO;
 import memoWeb.web.main.service.ChatServeice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,44 +23,44 @@ import java.util.Map;
 @RequestMapping("/api/chat")
 public class ChatController {
 
-    private final ChatServeice chatServeice;
+	private final ChatServeice chatServeice;
 
-    @Autowired
-    public ChatController(ChatServeice chatServeice) {
-        this.chatServeice = chatServeice;
-    }
+	@Autowired
+	public ChatController(ChatServeice chatServeice) {
+		this.chatServeice = chatServeice;
+	}
 
-    @MessageMapping("/receive/{chatRoomIdx}")
-    @SendTo("/send/{chatRoomIdx}")
-    public ChatVO SocketHandler(ChatVO chatVO) {
-        String userName = chatVO.getUserName();
-        String content = chatVO.getContent();
+//	@MessageMapping("/receive/{chatRoomIdx}")
+//	@SendTo("/send/{chatRoomIdx}")
+//	public ChatVO SocketHandler(ChatVO chatVO) {
+//		String userName = chatVO.getUserName();
+//		String content = chatVO.getContent();
+//
+//		ChatVO result = new ChatVO(userName, content);
+//		return result;
+//	}
 
-        ChatVO result = new ChatVO(userName, content);
-        return result;
-    }
+	@GetMapping("/chat")
+	public Map<String, Object> getChatList(HttpSession session) {
+		Map<String, Object> resultMap = new HashMap<>();
+		UserDTO user = (UserDTO) session.getAttribute(CommonConstants.SESSION);
+		List<ChatRoomDTO> chatRoomList = chatServeice.getChatList(user);
+		resultMap.put("chatList", chatRoomList);
+		return resultMap;
+	}
 
-    @GetMapping("/chat")
-    public Map<String, Object> getChatList(HttpSession session) {
-        Map<String, Object> resultMap = new HashMap<>();
-        UserDTO user = (UserDTO) session.getAttribute(CommonConstants.SESSION);
-        List<ChatRoomDTO> chatRoomList = chatServeice.getChatList(user);
-        resultMap.put("chatList", chatRoomList);
-        return resultMap;
-    }
+	@GetMapping("/chatRoom/{userId}")
+	public Map<String, Object> getChatRoom(HttpSession session, @PathVariable String userId) {
+		Map<String, Object> resultMap = new HashMap<>();
+		HashMap<String, Object> params = new HashMap<>();
+		UserDTO user = (UserDTO) session.getAttribute(CommonConstants.SESSION);
+		params.put("user1", user.getUserId());
+		params.put("user2", userId);
+		ChatRoomUserDTO chatRoomInfo = chatServeice.getChatRoom(params);
+		UserDTO chatUserInfo = chatServeice.getUserInfo(userId);
 
-    @GetMapping("/chatRoom/{userId}")
-    public Map<String, Object> getChatRoom(HttpSession session, @PathVariable String userId) {
-        Map<String, Object> resultMap = new HashMap<>();
-        HashMap<String, Object> params = new HashMap<>();
-        UserDTO user = (UserDTO) session.getAttribute(CommonConstants.SESSION);
-        params.put("user1", user.getUserId());
-        params.put("user2", userId);
-        ChatRoomUserDTO chatRoomInfo = chatServeice.getChatRoom(params);
-        UserDTO chatUserInfo = chatServeice.getUserInfo(userId);
-
-        resultMap.put("chatRoomInfo", chatRoomInfo);
-        resultMap.put("chatUserInfo", chatUserInfo);
-        return resultMap;
-    }
+		resultMap.put("chatRoomInfo", chatRoomInfo);
+		resultMap.put("chatUserInfo", chatUserInfo);
+		return resultMap;
+	}
 }
