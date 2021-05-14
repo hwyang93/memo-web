6495d84b06e2a44478ccacb6cd6df3cf08f8cd40
hwyang93 <yanghw0911@gmail.com>
2021-05-14 09:45:40 +0900

    - 친구신청 수정

--- src/main/java/memoWeb/web/myGroup/service/MyGroupService.java
+++ src/main/java/memoWeb/web/myGroup/service/MyGroupService.java
@@ -14,68 +14,72 @@ import java.util.List;
 @Service
 public class MyGroupService {
 
-    @Autowired
-    private final MyGroupRepository myGroupRepository;
+	@Autowired
+	private final MyGroupRepository myGroupRepository;
 
-    public MyGroupService(MyGroupRepository myGroupRepository) {
-        this.myGroupRepository = myGroupRepository;
-    }
+	public MyGroupService(MyGroupRepository myGroupRepository) {
+		this.myGroupRepository = myGroupRepository;
+	}
 
-    public List<UserDTO> getUserList(HashMap<String, Object> params) {
-        return myGroupRepository.getUserList(params);
-    }
+	public List<UserDTO> getUserList(HashMap<String, Object> params) {
+		return myGroupRepository.getUserList(params);
+	}
 
-    public UserVO getUserInfo (UserVO user){
-        return myGroupRepository.getUserInfo(user);
-    };
+	public UserVO getUserInfo(UserVO user) {
+		return myGroupRepository.getUserInfo(user);
+	}
 
-    public UserRelationDTO joinUserRelation (UserRelationDTO userRelation) {
-        return myGroupRepository.joinUserRelation(userRelation);
-    }
+	;
 
-    public List<UserRelationDTO> getFriendList (UserRelationVO userRelation) {
-        return myGroupRepository.getFriendList(userRelation);
-    }
+	public long joinUserRelation(UserRelationDTO userRelation) {
+		return myGroupRepository.joinUserRelation(userRelation);
+	}
 
-    public List<GroupDTO> getGroupList(UserVO user) { return myGroupRepository.getGroupList(user); }
+	public List<UserRelationDTO> getFriendList(UserRelationVO userRelation) {
+		return myGroupRepository.getFriendList(userRelation);
+	}
 
-    public GroupsVO getGroupInfo (GroupsVO group) {
-        return myGroupRepository.getGroupInfo(group);
-    }
+	public List<GroupDTO> getGroupList(UserVO user) {
+		return myGroupRepository.getGroupList(user);
+	}
 
-    public List<GroupMemberDTO> getGroupMemberList(GroupDTO group) {
-        return myGroupRepository.getGroupMemberList(group);
-    }
+	public GroupsVO getGroupInfo(GroupsVO group) {
+		return myGroupRepository.getGroupInfo(group);
+	}
 
-    public int getGroupIdx() {
-        return myGroupRepository.getGroupIdx();
-    }
+	public List<GroupMemberDTO> getGroupMemberList(GroupDTO group) {
+		return myGroupRepository.getGroupMemberList(group);
+	}
 
-    public void createGroup(GroupsVO group) {
-         myGroupRepository.createGroup(group);
-    }
+	public int getGroupIdx() {
+		return myGroupRepository.getGroupIdx();
+	}
 
-    public void joinGroupMember(List<GroupMemberVO> groupMemberList) {
-        groupMemberList.forEach(myGroupRepository::joinGroupMember);
-    }
+	public void createGroup(GroupsVO group) {
+		myGroupRepository.createGroup(group);
+	}
 
-    public void deleteGroup(GroupDTO group) {
-        myGroupRepository.deleteGroupMembers(group);
-        myGroupRepository.deleteGroup(group);
-    }
+	public void joinGroupMember(List<GroupMemberVO> groupMemberList) {
+		groupMemberList.forEach(myGroupRepository::joinGroupMember);
+	}
 
-    public List<UserRelationDTO> getFriendReqList(UserDTO user) {
-        return myGroupRepository.getFriendReqList(user);
-    }
+	public void deleteGroup(GroupDTO group) {
+		myGroupRepository.deleteGroupMembers(group);
+		myGroupRepository.deleteGroup(group);
+	}
 
-    public void friendConsent (UserRelationDTO userRelation) {
-        userRelation.setRelationStatus("I");
-        UserRelationDTO joinInfo = new UserRelationDTO();
-        joinInfo.setUserId(userRelation.getFollowUserId());
-        joinInfo.setFollowUserId(userRelation.getUserId());
-        joinInfo.setRelationStatus("I");
-        long result = myGroupRepository.updateUserRelationStatus(userRelation);
-        myGroupRepository.joinUserRelation(userRelation);
-    }
+	public List<UserRelationDTO> getFriendReqList(UserDTO user) {
+		return myGroupRepository.getFriendReqList(user);
+	}
+
+	public void friendConsent(UserRelationDTO userRelation) {
+		userRelation.setRelationStatus("I");
+		UserRelationDTO joinInfo = new UserRelationDTO();
+		joinInfo.setUserId(userRelation.getFollowUserId());
+		joinInfo.setFollowUserId(userRelation.getUserId());
+		joinInfo.setRelationStatus("I");
+		long result = myGroupRepository.updateUserRelationStatus(userRelation);
+		myGroupRepository.joinUserRelation(userRelation);
+	}
 
 }
