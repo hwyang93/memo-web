e10d67b66aee6bb9337fee844df70c97c81d560d
hyeonseo-noh <hseo0112@naver.com>
2021-05-17 09:13:42 +0900

    admin작업중

---
+++ src/vue/src/components/adminTopMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="topMenu-a">
+    <div class="title-container title-light">
+      <div class="title-dash">
+        <a class="title-brand">Dashboard</a>
+        <a id="logoutBtn" @click="logout()">logout</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'adminTopMenu'
+};
+</script>
+<style>
+.title-container.title-light {
+  background-color: #f4f3ef;
+  box-shadow: none;
+  border-bottom: 1px solid #dddddd;
+}
+.title-dash {
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.title-brand {
+  font-weight: 600;
+  margin: 5px 0px;
+  padding: 22px 15px;
+  font-size: 14px;
+  color: #66615b;
+  text-transform: uppercase;
+  display: inline-block;
+  line-height: inherit;
+  white-space: nowrap;
+}
+#logoutBtn {
+  float: right;
+  margin: 5px 0px;
+  padding: 22px 15px;
+}
+</style>
